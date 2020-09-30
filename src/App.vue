9b7953e9826c79830bfd05ab87dffa4cf1d20525
Aleksey Nosikov <na@devspark.ru>
2020-09-30 19:22:35 +0300

    Configures eslint

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img
+            v-if="a"
+            alt="Vue logo"
+            src="./assets/logo.png"
+        >
+        <p></p>
+        <hello-world
+            msg="Welcome to Your Vue.js App"
+            @click="() => {}"
+        >
+            <p>1</p>
+        </hello-world>
+        <a
+            href="//yandex.ru"
+            target="_blank"
+        ></a>
+    </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+    data() {
+        return {
+            a: 1,
+        }
+    },
+    computed: {
+        b() {
+            return 1
+        },
+    },
+    watch: {
+        a(_, old) {
+            console.log(old)
+        },
+    },
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
