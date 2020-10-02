e68ede61fdd642fbb631e4e959dd3d41c265c6cc
Aleksey Nosikov <na@devspark.ru>
2020-10-02 19:00:32 +0300

    Adds stylelint

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,11 @@ export default {
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  margin-top: 60px;
 }
 </style>
