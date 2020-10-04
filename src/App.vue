6f28bd700f8a23a1f1ae8f612960820d89a9c7ee
gronula <gronula@yandex.ru>
2020-10-05 01:38:03 +0300

    COnfigures stylelint

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="app">
+    <div
+        id="app"
+        class="app"
+    >
         <img
             alt="Vue logo"
             src="./assets/logo.png"
@@ -20,12 +23,12 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #000;
-  margin-top: 60px;
+.app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #000000;
+    margin-top: 60px;
 }
 </style>
