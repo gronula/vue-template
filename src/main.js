61cb2ef7e31354eb3e5dfa60d72349f05adddcae
gronula <gronula@yandex.ru>
2020-10-06 00:09:40 +0300

    Updates

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import App from '@/App.vue'
+import store from '@/store'
+import PromisesBus from '@/utils/PromisesBus'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
+Vue.prototype.$promises = new PromisesBus()
 
 new Vue({
     name: 'Root',
